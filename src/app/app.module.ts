110800b0dd4410e7cdec8371e8bddf99f72d56ab
InduPruthi <er.indupruthi@gmail.com>
2018-02-14 12:55:27 +0530

    FirstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule
